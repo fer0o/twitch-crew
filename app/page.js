e382cf21befb588031ca47bc71b6ff8f42502f71
fer0o <fer0osennin@gmail.com>
2023-06-15 04:45:07 -0600

    pruebas con next13

--- app/page.js
+++ app/page.js
@@ -1,7 +1,9 @@
+
+
 export default function Home() {
   return (
-    <main>
-      <h1 className=" text-cyan-950 text-3xl font-bold underline">Hello world!</h1>
-    </main>
+    <div>
+        <h1>Inicio</h1>
+    </div>
   );
 }
---
+++ components/Navbar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>esto es una navbar</div>
+  )
+}
+
+export default Navbar
